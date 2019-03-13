8511bcfb34be19e84d0e480b17e21af2302fd039
Joe Zemmels <jzemmels@iastate.edu>
2019-03-13 17:31:54 -0500

    Pushing magrittr dependency.

--- R/team_8.R
+++ R/team_8.R
@@ -1,7 +1,7 @@
 team_8 <- function(file,tolerance=.1){
-  file_big <- read_sf(file)
+  file_big <- sf::read_sf(file)
   file_st <- maptools::thinnedSpatialPoly(as(file_big, "Spatial"), tolerance = tolerance, minarea = 0.001, topologyPreserve = TRUE)
-  file <- st_as_sf(file_st)
+  file <- sf::st_as_sf(file_st)
 
   # Helper fucntion to turn one of these matrices into a single data frame with variables long, lat, group, order.
   helper <- function(y) {
@@ -13,7 +13,7 @@ team_8 <- function(file,tolerance=.1){
   }
   geometry <- file$geometry
   # In order to receive list of matrices, we used the flatten
-  matrix <- geometry %>% flatten() %>% flatten()
+  matrix <- geometry %>% purrr::flatten() %>% purrr::flatten()
   # By using  purrr functionality, we create the dataframe
   fileplus <- matrix %>% purrr::map_df(.x = ., .id ="group", .f = helper)
 
