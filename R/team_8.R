6eeb6128b6f4ed5e6bc43c3035d9a4a81817e2ad
Joe Zemmels <jzemmels@iastate.edu>
2019-03-13 23:26:33 -0500

    Adding (hopefully) final function and tests.

--- R/team_8.R
+++ R/team_8.R
@@ -1,25 +1,44 @@
-#' Returns a data frame with group-indicated lat/long data
+#' Creates a data frame with group-indicated lat/long data
 #'
+#' From an input .shp file, will create a data frame with columns indicating the country and state/territory of which the data represent, latitude and longitude data, and the group to which the lat/long data belong. Perfectly set-up to put into a ggplot call.
 #'
+#' @export
+#' @param file a file path string
+#' @param tolerance the tolerance value in the metric of the input object
+#'
+#' @examples
+#' #oz <- team_8("tests/testData/team_8_testData.shp",tolerance=.1)
+#' #oz %>%
+#' # ggplot(aes(x=long,y=lat,group=group)) + geom_path()
+#'
+#' @importFrom magrittr %>%
 
 team_8 <- function(file,tolerance=.1){
-  file_big <- sf::read_sf(file)
-  file_st <- maptools::thinnedSpatialPoly(as(file_big, "Spatial"), tolerance = tolerance, minarea = 0.001, topologyPreserve = TRUE)
-  file <- sf::st_as_sf(file_st)
+  checkmate::assertCharacter(file)
+  checkmate::checkFileExists(file)
+  checkmate::assertNumber(tolerance)
 
-  stateNames <- file$NAME_1 %>% c() #vector of state/territory names
+  file_big <- sf::read_sf(file) %>% checkmate::assertClass(c("sf","tbl_df","tbl","data.frame"))
+  file_st <- maptools::thinnedSpatialPoly(as(file_big, "Spatial"), tolerance = tolerance, minarea = 0.001, topologyPreserve = TRUE) %>% checkmate::assertClass("SpatialPolygonsDataFrame")
+  file <- sf::st_as_sf(file_st) %>% checkmate::assertClass(c("sf","data.frame"))
+
+  stateName <- file$NAME_1 #state/territory names
   geomSizes <- file$geometry %>%
     purrr::map(.f= ~ length(purrr::flatten(purrr::flatten(.x)))) %>% #number of rows per geometry
-    purrr::flatten_int()/2 #For some reason it was outputting exactly twice the numbers it should have
+    purrr::flatten_int()/2 %>% #(I think) length is counting all of the x rows AND y rows above, so we need to divide by 2
+    checkmate::assertNumeric()
 
-  stateNames <- stateNames %>%
+
+  stateName <- stateName %>%
     purrr::map2(geomSizes,.f= ~ rep(.x,each=.y)) %>% #Repeat a state name the same number of times as rows in its geometry
     purrr::flatten_chr()
+  checkmate::assertCharacter(stateName)
 
   totalGeomLength <- sum(geomSizes)
   countryName <- file_big$NAME_0 %>%
     unique %>%
-    rep(times=totalGeomLength) #Repeat the country names as many times as rows in all geometries
+    rep(times=totalGeomLength)#Repeat the country names as many times as rows in all geometries
+  checkmate::assertCharacter(countryName)
 
   # Helper fucntion to turn one of these matrices into a single data frame with variables long, lat, group, order.
   helper <- function(y){ #Function from team 8's solution
@@ -28,11 +47,13 @@ team_8 <- function(file,tolerance=.1){
                        order = 1:nrow(y))
     data
   }
+
   fileplus <- file$geometry %>%
     purrr::flatten() %>%
     purrr::flatten() %>%
     purrr::map_df(.x = ., .id ="group", .f = helper) %>%
-    cbind(countryName,stateNames) #Append the state and country name vectors to the data frame
+    cbind(countryName,stateName) #Append the state and country name vectors to the data frame
+  checkmate::assertDataFrame(fileplus)
 
   fileplus
 }
